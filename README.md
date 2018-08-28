359f621df49d59229fa378151f34fbf814097b30
Connor Yates <yatesco@oregonstate.edu>
2018-08-28 14:05:03 -0700

    Basic structure for the q learner package, set up to install via pip

---
+++ README.md
@@ -0,0 +1,5 @@
+#Q Learner in Python
+A simple, clean, readable implementation of a standard Q-Learner in Python 3. 
+Designed to be used in future research when I need a clean Q-Learner implementation, or to create variations of discrete Q-Learning.
+
+##Structure
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="QLearner",
+        version="0.1",
+        description="A basic Q-Learner class.",
+        url="https://github.com/Sir-Batman/QLearner",
+        author="Connor Yates",
+        author_email="yatesco@oregonstate.edu",
+        license="MIT",
+        packages=["qlearner"],
+        zip_safe=False)
