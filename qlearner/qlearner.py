29efea25de8fe8518d6b12d7b2dac2f8852958f5
Connor Yates <yatesco@oregonstate.edu>
2018-08-29 14:24:55 -0700

    Finish Q Learner implementation
    This completes the baseline work on implementing the Q learner agent
    class. Next steps include validating the Q learning aspect by writing
    tests (via pytest) for the package.

--- qlearner/qlearner.py
+++ qlearner/qlearner.py
@@ -19,30 +19,57 @@ class QLearner(object):
     Q Learning Agent Class.
     Contains the Q Table, update functions, and action selection.
     """
-    def __init__(self):
+    def __init__(self, num_states, num_actions, alpha=0.1, gamma=0.9, epsilon=0.1):
         """
-
         """
-        raise NotImplemented
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self._q_table = np.zeros((num_states, num_actions))
 
-    def initializeQValues(self):
-        raise NotImplemented
+        # Learning Parameters
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
 
-    def updateQvalue(self, PreviousState, selectedAction, currentState, reward):
-        raise NotImplemented
+    def updateQvalue(self, previous_state, selected_action, current_state, reward):
+        old_q = self._q_table[previous_state][selected_action]
+        max_q = self.getMaxQValue(current_state)
+        new_q = old_q + alpha*(reward + gamma*max_q - old_q)
+        self._q_table[previous_state][selected_action] = new_q
 
     def selectAction(self, state):
-        raise NotImplemented
+        """
+        Epsilon-greedy action selection.
+
+        Args:
+            state: Integer, the state the agent sees.
+
+        Returns: Integer, action the agent has selected
+        """
+        if random.random() < self.epsilon:
+            # Select random action with probabilty epsilon
+            return self.randomAction()
+        else:
+            # Select most valueable action
+            return self.getMaxValueAction(state)
 
     def randomAction(self):
         """ Returns a random action from the action space. """
         return random.randint(0, self.numActions)
 
     def getMaxValueAction(self, state):
-        raise NotImplemented
+        return np.argmax(self._q_table[state])
 
     def getMaxQValue(self, state):
-        raise NotImplemented
+        """
+        Returns the actual Q Value for the max action at this state.
+        Args:
+            state: Integer, the state the agent sees.
+
+        Returns: The Max Q value for this state.
+        """
+        best_action = self.getMaxValueAction(state)
+        return self._q_table[state][best_action]
 
     def saveQTable(self):
         """
